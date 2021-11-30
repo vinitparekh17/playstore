d7eedb0b39263c72913d85fe1b9dac3a655a9955
Vinit Parekh <77158947+vinitsoni17@users.noreply.github.com>
2021-11-30 19:39:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const playstore = require("google-play-scraper");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", (req, res) => {
+
+    playstore.search({
+        term: req.body.s,
+        num: 1
+
+    }).then(Data => {
+        let App;
+
+        try {
+            App = JSON.parse(JSON.stringify(Data[0]));
+        } catch (error) {
+            return res.write("<h1> Error 404: </h1> <br /> <h2> Application not found!</h2>")
+        }
+        var name = App.title;
+        var icon = App.icon;
+        var description = App.summary;
+        var dev = App.developer;
+        var url = App.url;
+        var value = App.priceText;
+        var score = App.scoreText;
+
+        res.render('result',
+            {
+                appName: name,
+                appDescription: description,
+                appIcon: icon,
+                appValue: value,
+                appScore: score,
+                appUrl: url,
+                appDev: dev
+            })
+    })
+})
+
+app.listen(5500, (req, res) => {
+    console.log("http://localhost:5500/")
+})
